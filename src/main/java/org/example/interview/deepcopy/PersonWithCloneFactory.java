f85c2a7cbaeda9617fcdc26b138cea578284b789
TommyWU <tommy111811@gmail.com>
2023-12-16 17:52:32 +0800

    [20231216]-深拷貝與淺拷貝

---
+++ src/main/java/org/example/interview/deepcopy/PersonWithCloneFactory.java
@@ -0,0 +1,37 @@
+package org.example.interview.deepcopy;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PersonWithCloneFactory {
+
+    private String name;
+    private int age;
+    private AddressWithCloneFactory address;
+
+    /** 給 Person1 使用的 */
+    public PersonWithCloneFactory(PersonWithCloneFactory original) {
+        this.name = original.name;
+        this.address = new AddressWithCloneFactory(original.address.getCity(), original.address.getStreet());
+        this.age = original.age;
+    }
+
+    /** 給 Person2 使用的 */
+    static PersonWithCloneFactory newInstance(PersonWithCloneFactory original) {
+        return new PersonWithCloneFactory(original.name, original.age, new AddressWithCloneFactory(original.address.getCity(), original.address.getStreet()));
+    }
+}
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+class AddressWithCloneFactory {
+
+    private String city;
+    private String street;
+
+}
---
+++ src/main/java/org/example/interview/deepcopy/PersonWithCloneFactoryTest.java
@@ -0,0 +1,28 @@
+package org.example.interview.deepcopy;
+
+/**
+ * 這邊測試的是 使用 newInstance 這類的靜態工廠方法達到深拷貝
+ */
+public class PersonWithCloneFactoryTest {
+    public static void main(String[] args) {
+        AddressWithCloneFactory address = new AddressWithCloneFactory("LA", "la");
+        PersonWithCloneFactory original = new PersonWithCloneFactory("LA Boy", 23, address);
+        // 使用 構造器
+        PersonWithCloneFactory copyUsingConstructor = new PersonWithCloneFactory(original);
+        // 使用 靜態工廠方法
+        PersonWithCloneFactory copyUsingFactory = PersonWithCloneFactory.newInstance(copyUsingConstructor);
+
+        System.out.println("==============Original============");
+        System.out.println(original);
+        System.out.println(copyUsingConstructor);
+        System.out.println(copyUsingFactory);
+        System.out.println("==============After change============");
+        copyUsingConstructor.setAge(24);
+        copyUsingConstructor.setName("SF Girl");
+        copyUsingConstructor.getAddress().setCity("SF");
+        System.out.println(original);
+        System.out.println(copyUsingConstructor);
+        System.out.println(copyUsingFactory);
+    }
+
+}
